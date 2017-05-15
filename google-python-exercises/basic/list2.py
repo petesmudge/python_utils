8c66be50b1a5891c8aedd607c54d1b90226d8f78
peter.smith <peter.smith@vertu.com>
2017-05-15 14:37:44 +0100

    Initial commit, localisation script runs okay on android l workarea

---
+++ google-python-exercises/basic/list2.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+import time
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print '%s function took %0.9f ms' % (f.func_name, (time2-time1)*1000.0)
+        return ret
+    return wrap
+# Additional basic list exercises
+
+# D. Given a list of numbers, return a list where
+# all adjacent == elements have been reduced to a single element,
+# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
+# modify the passed in list.
+def remove_adjacent(nums):
+  # +++your code here+++
+  i = 0
+  while i < (len(nums) -1) :
+    if nums[i] == nums[i+1]:
+      nums.pop(i)
+    else:
+      i += 1
+  return nums
+
+
+# E. Given two lists sorted in increasing order, create and return a merged
+# list of all the elements in sorted order. You may modify the passed in lists.
+# Ideally, the solution should work in "linear" time, making a single
+# pass of both lists.
+def linear_merge(list1, list2):
+  # +++your code here+++
+  return sorted(list1 + list2)
+  result = []
+  # Look at the two lists so long as both are non-empty.
+  # Take whichever element [0] is smaller.
+  while len(list1) and len(list2):
+    if list1[-1] > list2[-1]:
+      result.append(list1.pop(-1))
+    else:
+      result.append(list2.pop(-1))
+
+  # Now tack on what's left
+  list1.reverse()
+  list2.reverse()
+  result.extend(list1)
+  result.extend(list2)
+  result.reverse()
+  return result
+
+# Note: the solution above is kind of cute, but unforunately list.pop(0)
+# is not constant time with the standard python list implementation, so
+# the above is not strictly linear time.
+# An alternate approach uses pop(-1) to remove the endmost elements
+# from each list, building a solution list which is backwards.
+# Then use reversed() to put the result back in the correct order. That
+# solution works in linear time, but is more ugly.
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+@timing
+def loop():
+  for i in range(10000):
+    linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
+
+# Calls the above functions with interesting inputs.
+def main():
+  print 'remove_adjacent'
+  test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+  test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+  test(remove_adjacent([]), [])
+
+  print
+  print 'linear_merge'
+  print time
+  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+       ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+  loop()
+
+
+if __name__ == '__main__':
+  main()
---
+++ localisation/localisatio.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python -tt
+
+import os,sys,re
+global_except_list = ['.git','tests','overlays', 'examples']
+except_list = ['kernel','bootable','docs','build','bionic','abi','out','cts','development','pdk','tools','sdk']
+#deliberately remove en-rGB as it is not well localised!
+languages = [ 'ar', 'cs', 'de', 'es', 'es-rUS', 'fr', 'hu', 'in', 'it', 'iw', 'ja', 'ko',
+              'nb', 'nl', 'pt', 'ru', 'th','tr', 'uk', 'vi', 'zh-rCN', 'zh-rTW']
+#good for Titan
+overlay_root = "vendor/Vertu/overlays/common"
+
+def findResFiles(wa):
+  reslist = []
+  for basedir,dirs,files in os.walk(wa):
+    #print basedir
+    if basedir == wa:
+      print basedir
+      for folder in except_list:
+        if folder in dirs: dirs.remove(folder)
+
+    #filter out any generic folders not to be searched/checked
+    for folder in global_except_list:
+      if folder in dirs: dirs.remove(folder)
+      	
+    #print basedir, dirs
+    if re.search(r'res/values$',basedir):
+      #print basedir
+      for file in files:
+        #print file
+        if re.search(r'^strings.xml',file):
+          reslist.append(os.path.join(basedir,file))
+  return reslist
+
+# from one "string.xml", generate output for this component
+def findResComponent(filename):
+  (dir, name) = os.path.split(filename)
+  #print dir
+
+  inp = open(filename, 'rU')
+  #w_inp = inp.read()
+  string_names = []
+  for line in inp:
+    match = re.search(r'<string name="(\S+?)"',line)
+    # discard the non translatable strings
+    if match:
+      match2 = re.search(r'translatable="false"',line)
+      if not match2:
+        string_names.append(match.group(1))
+
+  # get all resource names and strings? (do we just need resource names?)
+  #string_names = re.findall(r'<string name="(\S+?)"', w_inp)
+  #
+
+  inp.close()
+
+  #print string_names
+  missing_dict = {}
+  #take "values" from the path
+  for basedir, dirs, files in os.walk(os.path.join(dir,'..')):
+    # find localised folders
+    #print basedir
+    match = re.search(r'../values-(\S+)', basedir)
+    if match and match.group(1) in languages:
+      in_fn = os.path.join(basedir, 'strings.xml')
+      if os.path.isfile(in_fn):
+        loc_file = open(in_fn,'rU')
+        #print in_fn
+        all_file = loc_file.read()
+        loc_file.close()
+        #look for overlay and append it to the file if exists
+        ov_fn = os.path.join(overlay_root,in_fn)
+        if os.path.isfile(ov_fn):
+          #print "Overlay found : " + ov_fn
+          ov_file = open(ov_fn)
+          all_file += ov_file.read()
+          ov_file.close()
+
+        # now search over the file for all occurences of each component of string_names
+        # only care about missing ones - once found, move on - store locale value for missing item into dict indexed by string_name
+        # { string_name, ['en','fr','de']}   < only entries for those localisations missing.
+        # optional - keep stats (i.e. number matched, total, number missing
+        # TBD #
+        #print match.group(0)
+        # maybe re whole list to get list of components, then match lists?
+        loc_string_names = re.findall(r'<string name="(\S+?)"', all_file)
+        #now just match names?
+        i = 0
+        for strg in string_names:
+          if strg not in loc_string_names:
+            #print strg
+            # add the missing string into the dict, with the language code
+            if strg not in missing_dict:
+              missing_dict[strg] = [match.group(1)]
+            else:
+              missing_dict[strg].append(match.group(1))
+          else:
+            i += 1
+            #print i
+          #print strg
+        #print 'processed ' + filename + ' with ' + str(i) + ' matches out of ' + str(len(string_names))
+        # TBD#
+        #sys.exit(0)
+
+  return missing_dict
+
+def printFileResults(file,resdict,level):
+  non_trans = 0
+  lst = []
+  if resdict:
+    print "%s: %d partially translated strings:" % (file,len(resdict.keys()))
+    for key in sorted(resdict.keys()):
+      if level < 2:
+        print key, resdict[key]
+      elif len(resdict[key]) == len(languages):
+        non_trans += 1
+        lst.append(key)
+    if lst:
+      print " of which %d non translated: " % non_trans
+      print lst
+
+# given a list of string xml files and paths, find all string resource id's
+# then match through all localised folders, storing if it is not found
+def findMissingResources(reslist,level):
+  for file in reslist:
+    missing = findResComponent(file)
+    printFileResults(file,missing,level)
+  return
+
+
+def main():
+  args = sys.argv[1:]
+
+  if not len(args) == 2:
+    print 'usage: <path to workarea root i.e. android root> <level of output>'
+    print ' level 0 = print all stats, level 1 = list files that miss some translations,' \
+          ' level 2 = only files with NO translations '''
+    sys.exit(1)
+
+  workarea = args[0]
+  level = args[1]
+
+  if not os.path.exists(workarea):
+    print 'invalid path to workarea'
+    sys.exit(1)
+
+  all_res_files = findResFiles(workarea)
+  print all_res_files
+  print 'Out of %d components found : ' % len(all_res_files)
+
+  findMissingResources(all_res_files,level)
+
+if __name__ == '__main__':
+  main()
