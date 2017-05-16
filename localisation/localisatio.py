cdc1780bef5e79927b320d5c548f87fc9f65e1f7
peter.smith <peter.smith@vertu.com>
2017-05-16 16:26:36 +0100

    working for L and M

--- localisation/localisatio.py
+++ localisation/localisatio.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python -tt
 
 import os,sys,re
-global_except_list = ['.git','tests','overlays', 'examples']
+
+
+#modify these filter lists as appropriate to avoid searching no-relevant code
+#checked/removed on every search path:
+global_except_list = ['.git','tests', 'examples']
+#checked on top level only
 except_list = ['kernel','bootable','docs','build','bionic','abi','out','cts','development','pdk','tools','sdk']
-#deliberately remove en-rGB as it is not well localised!
+#list of langauges required for Vertu products
+#deliberately remove en-rGB as it is not well localised! tends to roll back to default anyway
+# can reduce this to reduce search scope for specific languages if required
 languages = [ 'ar', 'cs', 'de', 'es', 'es-rUS', 'fr', 'hu', 'in', 'it', 'iw', 'ja', 'ko',
               'nb', 'nl', 'pt', 'ru', 'th','tr', 'uk', 'vi', 'zh-rCN', 'zh-rTW']
-#good for Titan
-overlay_root = "vendor/Vertu/overlays/common"
 
-glob_non_trans_comp = 0
+#Find in project defintition - DEVICE_PACKAGE_OVERLAYS and PRODUCT_PACKAGE_OVERLAYS
+#added paths for tron and titan here
+overlay_roots = ["vendor/Vertu/overlays/common",
+                 #"vendor/Vertu/overlays/china",
+                 "device/qcom/common/device/overlay",
+                 "device/tct/tron/overlay",
+                 "custo_wimdata_ng/wlanguage/overlay"]
+
+string_search = r'<string\s*name\s*=\s*"(\S+?)"'
+
+glob_part_trans = 0
+glob_non_trans = 0
 
 def findResFiles(wa):
   reslist = []
   for basedir,dirs,files in os.walk(wa):
-    #print basedir
+
     if basedir == wa:
-      print basedir
       for folder in except_list:
         if folder in dirs: dirs.remove(folder)
 
@@ -24,20 +39,21 @@ def findResFiles(wa):
     for folder in global_except_list:
       if folder in dirs: dirs.remove(folder)
       	
-    #print basedir, dirs
+    #find files to parse for strings   
     if re.search(r'res/values$',basedir):
-      #print basedir
+
       for file in files:
-        #print file
-        if re.search(r'^strings.xml',file):
+
+        if re.search(r'^strings\S*.xml',file):
           reslist.append(os.path.join(basedir,file))
   return reslist
 
-# from one "string.xml", generate output for this component
+# from one "string.xml", generate stats on partial translations for this files
 def findResComponent(filename):
   (dir, name) = os.path.split(filename)
   #print dir
 
+  #1. First of all create a list of string components that are translateable
   inp = open(filename, 'rU')
   #w_inp = inp.read()
   string_names = []
@@ -46,58 +62,55 @@ def findResComponent(filename):
   dnt = False
   for line in inp:
 
-    dnt_match = re.search(r'[Dd]o not translate',line)
+    dnt_match = re.search(r'do not translate',line.lower())
 
-    match = re.search(r'<string name="(\S+?)"',line)
+    match = re.search(string_search,line)
     # discard the non translatable strings
     if match and not dnt:
-      match2 = re.search(r'translatable="false"',line)
+      match2 = re.search(r'translatable\s*=\s*"false"',line)
       if not match2:
         string_names.append(match.group(1))
     #set dnt true here
     if dnt_match : dnt = True
 
-
-  # get all resource names and strings? (do we just need resource names?)
-  #string_names = re.findall(r'<string name="(\S+?)"', w_inp)
-  #
-
   inp.close()
 
-  #print string_names
+  #2. now go through all sub-paths to find localised files
+  # in folders "values-*", apply any overlays,
+  #and then compare to the list of vanilla strings to find 
+  #if there are any non-translated
   missing_dict = {}
   #take "values" from the path
   upper_dir = os.path.dirname(dir)
   for basedir, dirs, files in os.walk(upper_dir):
     # find localised folders
-    #print basedir
     match = re.search(r'values-(\S+)', basedir)
     if match and match.group(1) in languages:
-      in_fn = os.path.join(basedir, 'strings.xml')
+      #find all files of the same name
+      in_fn = os.path.join(basedir, name)
       if os.path.isfile(in_fn):
         loc_file = open(in_fn,'rU')
         #print in_fn
         all_file = loc_file.read()
         loc_file.close()
-        #look for overlay and append it to the file if exists
-        ov_fn = os.path.join(overlay_root,in_fn)
-        #print ov_fn
-        if os.path.isfile(ov_fn):
-          #print "Overlay found : " + ov_fn
-          ov_file = open(ov_fn)
-          all_file += ov_file.read()
-          ov_file.close()
+        #look for overlay file and append it to the file if exists
+        for root in overlay_roots:
+          ov_fn = os.path.join(root,in_fn)
+          #print ov_fn
+          if os.path.isfile(ov_fn):
+            #print "Overlay found : " + ov_fn
+            ov_file = open(ov_fn)
+            all_file += ov_file.read()
+            ov_file.close()
+
+        # create list of all string components in localised files
+        loc_string_names = re.findall(string_search, all_file)
 
         # now search over the file for all occurences of each component of string_names
-        # only care about missing ones - once found, move on - store locale value for missing item into dict indexed by string_name
+        # store locale value for missing item into dict indexed by string_name
         # { string_name, ['en','fr','de']}   < only entries for those localisations missing.
-        # optional - keep stats (i.e. number matched, total, number missing
-        # TBD #
-        #print match.group(0)
-        # maybe re whole list to get list of components, then match lists?
-        loc_string_names = re.findall(r'<string name="(\S+?)"', all_file)
-        #now just match names?
-        i = 0
+     
+        #now just match names
         for strg in string_names:
           if strg not in loc_string_names:
             #print strg
@@ -106,33 +119,35 @@ def findResComponent(filename):
               missing_dict[strg] = [match.group(1)]
             else:
               missing_dict[strg].append(match.group(1))
-          else:
-            i += 1
-            #print i
-          #print strg
-        #print 'processed ' + filename + ' with ' + str(i) + ' matches out of ' + str(len(string_names))
-        # TBD#
-        #sys.exit(0)
 
   return missing_dict
 
 def printFileResults(file,resdict,level):
-  non_trans = 0
-  lst = []
+  non_lst = []
+  part_lst = []
+  global glob_non_trans
+  global glob_part_trans
+  #generate 2 lists, partial and not translated
+  # print both for level 1, and not translated only for level 2
   if resdict:
-    if level < 2 : print "\n%s: %d partially translated strings:" % (file,len(resdict.keys()))
     for key in sorted(resdict.keys()):
-      if level < 2:
+      if len(resdict[key]) == len(languages):
+        non_lst.append(key)
+      else:
+	part_lst.append(key)
+
+    if level < 2 and part_lst : 
+      print "\n%s : %d partially translated strings:" % (file,len(part_lst))
+      glob_part_trans += 1
+      for key in part_lst:
         print key, resdict[key]
-      elif len(resdict[key]) == len(languages):
-        non_trans += 1
-        lst.append(key)
-    if lst:
-      if level == "2" : print "\n%s : %d non_translated :" % (file,non_trans)
-      else : print " of which %d non translated: " % non_trans
-      print lst
-      global glob_non_trans_comp
-      glob_non_trans_comp += 1
+
+    if non_lst:
+      if level == 2 or not part_lst : print "\n%s" % file
+      print "\n%d completely non_translated :" % len(non_lst)
+      glob_non_trans += 1
+      print non_lst
+
 
 # given a list of string xml files and paths, find all string resource id's
 # then match through all localised folders, storing if it is not found
@@ -149,11 +164,11 @@ def main():
   if not len(args) == 2:
     print 'usage: <path to workarea root i.e. android root> <level of output>'
     print ' level 0 = print all stats, level 1 = list files that miss some translations,' \
-          ' level 2 = only files with NO translations '''
+          ' level 2 = list only files with NO translations '''
     sys.exit(1)
 
   workarea = args[0]
-  level = args[1]
+  level = int(args[1])
 
   if not os.path.exists(workarea):
     print 'invalid path to workarea'
@@ -165,8 +180,10 @@ def main():
 
   findMissingResources(all_res_files,level)
 
-  global glob_non_trans_comp
-  print "\n%d components have non translated strings" % glob_non_trans_comp
+  global glob_non_trans
+  global glob_part_trans
+  if level < 2: print "\n%d components have partially translated strings" % glob_part_trans
+  print "\n%d components have non translated strings" % glob_non_trans
 
 if __name__ == '__main__':
   main()
